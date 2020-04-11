afb8721e6ce4c504d35e46b3fe7c177115ecfe4a
Arjun <arjun.solanki@dignizant.com>
2020-04-11 14:13:23 +0530

    test changes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group(['middleware' => 'cors'], function () {
     Route::post('/submit', 'FormController@submit');
 });
+
